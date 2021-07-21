9a6c58be152f078db9b5982b47b99e8095decb48
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-21 16:53:37 -0700

    add heroku port and some app.gets for html pages

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 const path = require('path');
-const database = require('./db/db.json')
+const database = require('./db/db.json');
 
 app.use(express.static('public'));
 
+//GET route for homepage
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes on port ${PORT}!`)
